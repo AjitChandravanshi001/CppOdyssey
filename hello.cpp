1465c55a64e097982c099f5cf9818d4329b5d5b5
Ajit <131857733+AjitChandravanshi001@users.noreply.github.com>
2024-07-19 14:00:23 +0530

    Update hello.cpp
    Add functions

--- hello.cpp
+++ hello.cpp
@@ -4,3 +4,15 @@ int main() {
     std::cout << "Hello World!";
     return 0;
 }
+
+//using functions
+#include <iostream>
+
+void printHelloWorld() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() {
+    printHelloWorld();
+    return 0;
+}
